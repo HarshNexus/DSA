a996dc005281da64f5db449077689421772a1a91
Harsh kumar <23bcs11475@cuchd.in>
2026-02-03 18:11:20 +0530

    added new file

--- lec12/binarysearch.cpp
+++ lec12/binarysearch.cpp
@@ -1,23 +1,6 @@
 #include<iostream>
 using namespace std;
-int binary(int arr[],int n,int f){
-    int s=0;
-    int e=n-1;
-    int mid=s+(e-s)/2;
-    while(s<=e){
-        mid=s+(e-s)/2;
-        if(arr[mid]==f){
-            return mid;
-        }
-        else if(arr[mid]>f){
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
-    }
-    return -1;
-}
+ 
 int main(){
     int arr[4]={1,2,3,4};
     int ans=binary(arr,4,3);
