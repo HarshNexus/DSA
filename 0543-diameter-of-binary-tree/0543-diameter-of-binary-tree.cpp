077e50f1741865ae489657f2a82f1c032d5e496b
Harsh kumar <23bcs11475@cuchd.in>
2026-06-03 23:19:50 +0530

    Time: 448 ms (5.01%), Space: 24.1 MB (7.83%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -10,27 +10,26 @@
  * };
  */
 class Solution {
-    private:
-    pair<int,int> dfast(TreeNode* root){
-        if(root == NULL){
-            pair<int,int> p=make_pair(0,0);
-            return p;
-        }
+private:
+int h(TreeNode* root){
+    if(root ==NULL) return 0;
 
-        pair<int,int>l=dfast(root->left);
-        pair<int,int>r=dfast(root->right);
+    int r=h(root->right);
+    int l=h(root->left);
+    int ans=max(l,r)+1;
+    return ans;
 
-        int o1=l.first;
-        int o2=r.first;
-        int o3=l.second+r.second;
-
-        pair<int,int>ans;
-        ans.first=max(o1,max(o2,o3));
-        ans.second=max(l.second,r.second)+1;
-        return ans;
-    }
+}
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        return dfast(root).first;
+        if(root==NULL) return 0;
+
+        int a=diameterOfBinaryTree(root->left);
+        int b=diameterOfBinaryTree(root->right);
+        int c=h(root->left)+h(root->right);
+
+        int ans=max(a,max(b,c));
+        return ans;
+
     }
 };
