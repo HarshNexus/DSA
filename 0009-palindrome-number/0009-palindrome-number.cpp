d40aae8b7dd302a4f634347b4de12747caae1309
Harsh kumar <23bcs11475@cuchd.in>
2026-06-12 19:54:56 +0530

    Time: 12 ms (5.03%), Space: 10.7 MB (19.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int reverse(int n){
-         long int reverse = 0;
-        while(n!=0){
-            int digit = n%10;
-            reverse=reverse*10+digit;
-            n=n/10;
-        }
-        return reverse;
-    }
     bool isPalindrome(int x) {
-        if(x>=0 && reverse(x)==x){
-            return 1;
-
+        if(x < 0) return false;
+        int st=0;
+        string s=to_string(x);
+        int e=s.size()-1;
+        while(st<e){
+            if(s[st]!=s[e]) return false;
+            st++;
+            e--;
         }
-        return 0;
+        return true;
     }
 };
