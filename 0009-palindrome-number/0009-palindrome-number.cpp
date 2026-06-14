22859c33b50cc8d9b050be75d848e099e98535df
Harsh kumar <23bcs11475@cuchd.in>
2026-06-14 21:10:22 +0530

    Time: 2 ms (50.93%), Space: 8.5 MB (65.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -12,16 +12,6 @@ int reverse(int n){
 
     bool isPalindrome(int x) {
         if(x < 0) return false;
-        // int st=0;
-        // string s=to_string(x);
-        // int e=s.size()-1;
-        // while(st<e){
-        //     if(s[st]!=s[e]) return false;
-        //     st++;
-        //     e--;
-        // }
-        // return true;
-
         return reverse(x)==x;
     }
 };
