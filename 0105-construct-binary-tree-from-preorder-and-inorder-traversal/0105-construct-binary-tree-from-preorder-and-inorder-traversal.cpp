89b498c115148045f8460bc64f00e65cce0b964d
Harsh kumar <23bcs11475@cuchd.in>
2026-06-15 22:18:28 +0530

    Time: 0 ms (100%), Space: 28.6 MB (11.01%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+void maping(vector<int>& inorder,int n,map<int,int> & nti){
+    for(int i=0;i<n;i++){
+        nti[inorder[i]]=i;
+    }
+}
+TreeNode* solve(vector<int>& preorder,vector<int>& inorder,int s,int e,int n,map<int,int> & nti,int &in){
+    if(in>=n || s>e){
+        return NULL;
+    }
+
+    int element =preorder[in++];
+    TreeNode*root=new TreeNode(element);
+    int pos=nti[element];
+
+    root->left=solve(preorder,inorder,s,pos-1,n,nti,in);
+    root->right=solve(preorder,inorder,pos+1,e,n,nti,in);
+    return root;
+}
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n=inorder.size();
+        int prein=0;
+        map<int,int>  nti;
+        maping(inorder,n,nti);
+        TreeNode*ans=solve(preorder,inorder,0,n-1,n,nti,prein);
+        return ans;
+
+    }
+};
