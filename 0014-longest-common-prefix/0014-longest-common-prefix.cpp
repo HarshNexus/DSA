90ef2e4a3c928f22de807b183664c9f88dd6e496
Harsh kumar <23bcs11475@cuchd.in>
2026-07-14 19:00:41 +0530

    Time: 0 ms (100%), Space: 11.9 MB (58.07%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        string ans ="";
         for(int i=0;i<strs[0].size();i++){
             char ch=strs[0][i];
+    
+    bool a=true;
             for(int j=1;j<strs.size();j++){
-                if(i>=strs[j].size() || strs[j][i]!=ch){
-                    return strs[0].substr(0,i);
+                if(i>strs[j].size() || ch!=strs[j][i]){
+                    a=false;
+                    break;
                 }
+
+            }
+            if(a==false){
+                break;
+            }
+            else{
+                ans.push_back(ch);
             }
         }
-        return strs[0];
-
+        return ans;
     }
 };
